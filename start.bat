226b0915c21a43d5f844dba9b8e4b0fd3a2fa0af
brpeanut <bresheske@gmail.com>
2016-03-09 09:54:34 -0600

    added find.

--- start.bat
+++ start.bat
@@ -2,4 +2,6 @@ echo off
 color 0a
 doskey ls=dir $*
 doskey grep=findstr /A:0F /s /n /r $0
+doskey grepf=dir /s /b $0 | findstr /A:0F /s /n /r $1
+doskey find=dir /s /b $*
 doskey mp3=D:\Programs\ffmpeg\bin\youtube-dl.exe --extract-audio --audio-format mp3 $*
